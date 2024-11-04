b6590ee6c77deea64a3bedc4dc6835d1306ce48f
ttak0422 <ttak0422@gmail.com>
2024-11-05 00:52:13 +0900

    refactor: deduplicate and convert to lua in export functions

--- bundler/src/bundle/export.rs
+++ bundler/src/bundle/export.rs
@@ -44,7 +44,11 @@ fn export_plugin_config(root: &str, cfg: PluginConfig) -> Result<()> {
 
     let packages_path = [root, constant::dir::PACKAGES, &id].join("/");
     let mut packages_file = create_or_open_file(packages_path)?;
-    write!(packages_file, "return {}", cfg.packages.into_lua())?;
+    write!(packages_file, "return {}", {
+        let mut ps = cfg.packages;
+        ps.dedup();
+        ps.into_lua()
+    })?;
 
     let startup_config_path = [root, constant::dir::STARTUP_CONFIGS, &id].join("/");
     let mut startup_config_file = create_or_open_file(startup_config_path)?;
@@ -70,7 +74,11 @@ fn export_plugin_config(root: &str, cfg: PluginConfig) -> Result<()> {
 
     let depends_path = [root, constant::dir::DEPENDS, &id].join("/");
     let mut depends_file = create_or_open_file(depends_path)?;
-    write!(depends_file, "return {}", cfg.depends.into_lua())?;
+    write!(depends_file, "return {}", {
+        let mut ps = cfg.depends;
+        ps.dedup();
+        ps.into_lua()
+    })?;
 
     Ok(())
 }
@@ -81,51 +89,64 @@ fn export_load_config(root: &str, cfg: LoadConfig) -> Result<()> {
     let module_keys_path = [root, constant::file::MODULE_KEYS].join("/");
     let mut module_keys_file = create_or_open_file(module_keys_path)?;
     write!(module_keys_file, "return {}", module_keys.into_lua())?;
-    for (m, ps) in cfg.on_modules {
+    for (m, mut ps) in cfg.on_modules {
+        ps.dedup();
         let module_path = [root, constant::dir::MODULES, m.as_ref()].join("/");
         let mut module_file = create_or_open_file(module_path)?;
         write!(module_file, "return {}", ps.into_lua())?;
     }
 
     // events
-    let event_keys = cfg.on_events.keys().collect::<Vec<_>>();
+    let event_keys = {
+        let mut ks = cfg.on_events.keys().collect::<Vec<_>>();
+        ks.dedup();
+        ks.into_lua()
+    };
     let event_keys_path = [root, constant::file::EVENT_KEYS].join("/");
     let mut event_keys_file = create_or_open_file(event_keys_path)?;
-    write!(event_keys_file, "return {}", event_keys.into_lua())?;
-    for (ev, ps) in cfg.on_events {
+    write!(event_keys_file, "return {}", event_keys)?;
+    for (ev, mut ps) in cfg.on_events {
+        ps.dedup();
         let event_path = [root, constant::dir::EVENTS, ev.as_ref()].join("/");
         let mut event_file = create_or_open_file(event_path)?;
         write!(event_file, "return {}", ps.into_lua())?;
     }
 
     // user events
-    let user_event_keys = cfg.on_userevents.keys().collect::<Vec<_>>();
+    let user_event_keys = {
+        let mut ks = cfg.on_userevents.keys().collect::<Vec<_>>();
+        ks.dedup();
+        ks.into_lua()
+    };
     let user_event_keys_path = [root, constant::file::USER_EVENT_KEYS].join("/");
     let mut user_event_keys_file = create_or_open_file(user_event_keys_path)?;
-    write!(
-        user_event_keys_file,
-        "return {}",
-        user_event_keys.into_lua()
-    )?;
-    for (ev, ps) in cfg.on_userevents {
+    write!(user_event_keys_file, "return {}", user_event_keys)?;
+    for (ev, mut ps) in cfg.on_userevents {
+        ps.dedup();
         let user_event_path = [root, constant::dir::USER_EVENTS, ev.as_ref()].join("/");
         let mut user_event_file = create_or_open_file(user_event_path)?;
         write!(user_event_file, "return {}", ps.into_lua())?;
     }
 
     // commands
-    let command_keys = cfg.on_commands.keys().collect::<Vec<_>>();
+    let command_keys = {
+        let mut ks = cfg.on_commands.keys().collect::<Vec<_>>();
+        ks.dedup();
+        ks.into_lua()
+    };
     let command_keys_path = [root, constant::file::COMMAND_KEYS].join("/");
     let mut command_keys_file = create_or_open_file(command_keys_path)?;
-    write!(command_keys_file, "return {}", command_keys.into_lua())?;
-    for (cmd, ps) in cfg.on_commands {
+    write!(command_keys_file, "return {}", command_keys)?;
+    for (cmd, mut ps) in cfg.on_commands {
+        ps.dedup();
         let command_path = [root, constant::dir::COMMANDS, cmd.as_ref()].join("/");
         let mut command_file = create_or_open_file(command_path)?;
         write!(command_file, "return {}", ps.into_lua())?;
     }
 
     // filetypes
-    for (ft, ps) in cfg.on_filetypes {
+    for (ft, mut ps) in cfg.on_filetypes {
+        ps.dedup();
         let ft_plugin_path = [
             root,
             constant::dir::AFTER,
@@ -149,26 +170,33 @@ fn export_load_config(root: &str, cfg: LoadConfig) -> Result<()> {
     }
 
     // startup
-    let startup_config_keys = cfg.startup_config_plugins.into_lua();
+    let startup_config_keys = {
+        let mut ps = cfg.startup_config_plugins;
+        ps.dedup();
+        ps.into_lua()
+    };
     let startup_config_keys_path = [root, constant::file::STARTUP_CONFIG_KEYS].join("/");
     let mut startup_config_keys_file = create_or_open_file(startup_config_keys_path)?;
     write!(startup_config_keys_file, "return {}", startup_config_keys)?;
 
     // denops
-    let denops_keys = cfg
-        .denops_clients
-        .into_iter()
-        .fold(HashMap::new(), |mut acc, k| {
-            acc.insert(k, true);
-            acc
-        })
-        .into_lua();
+    let denops_keys = {
+        let mut cs = cfg.denops_clients;
+        cs.dedup();
+        cs.into_iter()
+            .fold(HashMap::new(), |mut acc, k| {
+                acc.insert(k, true);
+                acc
+            })
+            .into_lua()
+    };
     let denops_keys_path = [root, constant::file::DENOPS_KEYS].join("/");
     let mut denops_keys_file = create_or_open_file(denops_keys_path)?;
     write!(denops_keys_file, "return {}", denops_keys)?;
 
     // rtp
-    for (id, paths) in cfg.rtp {
+    for (id, mut paths) in cfg.rtp {
+        paths.dedup();
         let rtp_path = [root, constant::dir::RTP, id.to_string().as_ref()].join("/");
         let mut rtp_file = create_or_open_file(rtp_path)?;
         write!(rtp_file, "return {}", paths.into_lua())?;
